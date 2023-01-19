ad47133219f5c770187d61b19ff8a3aa8628709d
Corwin Bridges <corwin.bridges@gmail.com>
2023-01-19 15:37:10 -0600

    getting rid of junk

--- party/src/App.jsx
+++ party/src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <h1 className="text-9xl font-bold underline flex justify-center items-center h-screen">
-      Hello world!
+      Hello, World!
     </h1>
   )
 }
