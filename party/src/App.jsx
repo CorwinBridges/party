9b63444081006a701a3c8e9667521768e8aa09c1
Madison Griffin <110925243+madisonbgriffin@users.noreply.github.com>
2023-01-19 08:50:08 -0600

    fixed

---
+++ party/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <h1 className="text-9xl font-bold underline flex justify-center items-center h-screen">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App;
