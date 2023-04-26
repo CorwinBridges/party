214d3037816bc7ca71593146ca981db61e68498f
Corwin Bridges <corwin.bridges@gmail.com>
2023-04-26 03:44:32 -0500

    fhzrhpz

--- party/frontend/postcss.config.cjs
+++ party/frontend/postcss.config.cjs
@@ -1,12 +1,6 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.jsx", "./src/**/*.js", "./public/index.html"],
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-})
-
 module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
