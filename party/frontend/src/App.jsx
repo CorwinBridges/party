3ceb953c53473ee17060cb4190d4711425b076ff
Corwin Bridges <corwin.bridges@gmail.com>
2023-04-28 07:43:53 -0500

    Added lazy loading

--- party/frontend/src/App.jsx
+++ party/frontend/src/App.jsx
@@ -1,21 +1,21 @@
+import { Suspense, lazy } from "react"
 import { Toaster } from "react-hot-toast"
 import { Route, Routes } from "react-router-dom"
 
 import { Footer, Navbar, BackToTop, NotFound } from "./components"
 import { StateContext } from "./context/StateContext"
 import "./index.css"
-import {
-  About,
-  Contact,
-  Home,
-  Shop,
-  Terms,
-  Privacy,
-  Returns,
-  Shipping,
-  Checkout,
-  Thanks,
-} from "./pages"
+
+const About = lazy(() => import("./pages/About/About"))
+const Contact = lazy(() => import("./pages/Contact/Contact"))
+const Home = lazy(() => import("./pages/Home/Home"))
+const Shop = lazy(() => import("./pages/Shop/Shop"))
+const Terms = lazy(() => import("./pages/Policies/Terms"))
+const Privacy = lazy(() => import("./pages/Policies/Privacy"))
+const Returns = lazy(() => import("./pages/Policies/Returns"))
+const Shipping = lazy(() => import("./pages/Policies/Shipping"))
+const Checkout = lazy(() => import("./pages/Checkout/Checkout"))
+const Thanks = lazy(() => import("./pages/Checkout/Thanks"))
 
 const App = () => {
   return (
@@ -24,19 +24,30 @@ const App = () => {
         <div className="section-container">
           <Navbar />
         </div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route exact path="/shop" element={<Shop />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/shipping" element={<Shipping />} />
-          <Route path="/returns" element={<Returns />} />
-          <Route path="/thanks" element={<Thanks />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="flex h-[50vh] items-center justify-center">
+              <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent text-pink-500 motion-reduce:animate-[spin_1.5s_linear_infinite]" />
+              <span className="ml-4 text-4xl text-white">
+                Loading Products...
+              </span>
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route exact path="/shop" element={<Shop />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/shipping" element={<Shipping />} />
+            <Route path="/returns" element={<Returns />} />
+            <Route path="/thanks" element={<Thanks />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
         <Footer />
         <BackToTop />
         {/* Toast */}
