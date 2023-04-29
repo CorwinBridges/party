c2242c28aa09888756e87f0504bd22a30cff916f
Corwin Bridges <corwin.bridges@gmail.com>
2023-04-29 09:17:11 -0500

    jxjv

--- party/frontend/vite.config.js
+++ party/frontend/vite.config.js
@@ -1,9 +1,8 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import viteCompression from "vite-plugin-compression"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react(), viteCompression()],
+  plugins: [react()],
 })
