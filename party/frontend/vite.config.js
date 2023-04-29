cd6acbe2a7a468f6a8dbbc10cd71f45fa1887ef5
Corwin Bridges <corwin.bridges@gmail.com>
2023-04-29 08:27:13 -0500

    gzip

--- party/frontend/vite.config.js
+++ party/frontend/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import viteCompression from "vite-plugin-compression"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()]
+  base: "/",
+  plugins: [react(), viteCompression()],
 })
